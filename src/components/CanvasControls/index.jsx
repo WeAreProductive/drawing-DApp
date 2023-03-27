50d3704a758fcfb7247bcd97ebbebcc4e91abd1d
Milena Tomova <kokolina1888@users.noreply.github.com>
2023-03-27 20:45:53 +0300

    Remove obsolete controls

--- src/components/CanvasControls/index.jsx
+++ src/components/CanvasControls/index.jsx
@@ -1,17 +1,11 @@
 import CanvasReset from "./CanvasReset";
-import CanvasFromJson from "./CanvasFromJson";
 import CanvasToJson from "./CanvasToJson";
-import Eraser from "./Eraser";
 const CanvasControls = () => {
   return (
     <div className="actions">
       <div>
-        <Eraser />
-        <CanvasReset />
-      </div>
-      <div>
         <CanvasToJson />
-        <CanvasFromJson />
+        <CanvasReset />
       </div>
     </div>
   );
