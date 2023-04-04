0492985db9686b620431ac2b26cc4e1381116c25
Milena Tomova <kokolina1888@users.noreply.github.com>
2023-04-04 11:40:39 +0300

    Update main.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./App"; 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+//Setup GraphQL Apollo client
+const URL_QUERY_GRAPHQL = "http://localhost:4000/graphql";
+
+const client = new ApolloClient({
+  uri: URL_QUERY_GRAPHQL,
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </ApolloProvider>
+);
