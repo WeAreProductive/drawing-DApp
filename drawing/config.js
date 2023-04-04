73481fffe34dbc4acd8963f61793f449823f4b80
Milena Tomova <kokolina1888@users.noreply.github.com>
2023-04-04 16:43:06 +0300

    Create config.js

---
+++ drawing/config.js
@@ -0,0 +1,4 @@
+export const API_KEY =
+  "n3xCRxOgEhZsOcrBGYsepjSFP1DXec1qlvjsoSdW9nk7KdyxHlXyc7OUIC4rGKPA";
+export const URL =
+  "https://eu-central-1.aws.data.mongodb-api.com/app/data-jhpfj/endpoint/data/v1";
