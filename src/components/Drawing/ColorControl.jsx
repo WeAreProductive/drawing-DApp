b7797f60b4286cbbb4c90804496a8bc85dfdc8c8
Milena Tomova <kokolina1888@users.noreply.github.com>
2023-04-13 11:42:37 +0300

    Use react-colorful to improve color picker UX

--- src/components/Drawing/ColorControl.jsx
+++ src/components/Drawing/ColorControl.jsx
@@ -1,25 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useCanvasContext } from "../../context/CanvasContext";
 import { INITIAL_DRAWING_OPTIONS } from "../../config/constants";
+import { HexColorPicker } from "react-colorful";
 const ColorControl = () => {
   const { canvasOptions, setOptions } = useCanvasContext();
   const [color, setColor] = useState(INITIAL_DRAWING_OPTIONS.color);
 
-  const handleColorChange = (e) => {
-    setColor(e.target.value);
-    setOptions({ ...canvasOptions, color: e.target.value });
-  };
+  useEffect(() => {
+    setOptions({ ...canvasOptions, color: color });
+  }, [color, canvasOptions.color]);
+  console.log(color);
+
   return (
     <div className="drawing-options color-controls">
-      <label htmlFor="drawing-color" className="control-label">
-        Line color:
-      </label>
-      <input
-        type="color"
-        value={color}
-        id="drawing-color"
-        onChange={handleColorChange}
-      />
+      <HexColorPicker color={color} onChange={setColor} />;
     </div>
   );
 };
