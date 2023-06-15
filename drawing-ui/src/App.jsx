fd2f6ce6c61522103c3d5a1f35f1e82908904437
Milena Tomova <kokolina1888@users.noreply.github.com>
2023-06-15 17:57:24 +0300

    Configure web3-onboard init

--- drawing-ui/src/App.jsx
+++ drawing-ui/src/App.jsx
@@ -6,6 +6,7 @@ import ImagesListRollups from "./components/ImagesRollups";
 import DrawingControls from "./components/Drawing/DrawingControls";
 import FabricJSCanvas from "./components/FabricJSCanvas";
 import CanvasControls from "./components/CanvasControls";
+import blocknativeIcon from "./icons/blocknative-icon"; //@TODO use app's own icon instead
 import "./App.css";
 
 import configFile from "./config/config.json";
@@ -13,8 +14,13 @@ import configFile from "./config/config.json";
 const config = configFile;
 
 const injected = injectedModule();
-
+//for more customizations look here
+// https://github.com/kokolina1888/react-demo
+// https://github.com/blocknative/react-demo/tree/master
 init({
+  connect: {
+    autoConnectAllPreviousWallet: true,
+  },
   wallets: [injected],
   chains: Object.entries(config).map(([k, v], i) => ({
     id: k,
@@ -22,15 +28,23 @@ init({
     label: v.label,
     rpcUrl: v.rpcUrl,
   })),
-
   appMetadata: {
     name: "Drawing DApp",
-    icon: "<svg><svg/>",
+    icon: blocknativeIcon,
     description: "Demo app for Cartesi Rollups",
     recommendedInjectedWallets: [
       { name: "MetaMask", url: "https://metamask.io" },
     ],
   },
+  accountCenter: {
+    desktop: {
+      position: "topRight",
+      enabled: true,
+      minimal: false,
+      // containerElement: "<div></div>", @TODO must be an existing DOM element selector ?!
+    },
+  },
+  // theme: "dark",
 });
 
 const App = () => {
