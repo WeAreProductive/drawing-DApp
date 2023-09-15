d992bfd2a1de3a02560b57379f9da0eeb346149a
Milena Tomova <kokolina1888@users.noreply.github.com>
2023-09-15 14:26:53 +0300

    FE: useRollups update

--- drawing-ui/src/hooks/useRollups.jsx
+++ drawing-ui/src/hooks/useRollups.jsx
@@ -13,31 +13,49 @@
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import { useSetChain, useWallets } from "@web3-onboard/react";
- 
+//  @TODO remove obsolete declarations
 
 import {
-  InputFacet__factory,
-  OutputFacet__factory,
-  RollupsFacet__factory,
+  CartesiDApp__factory,
+  InputBox__factory,
+  EtherPortal__factory,
+  ERC721Portal__factory,
+  DAppAddressRelay__factory,
 } from "../generated/rollups";
 
 import configFile from "../config/config.json";
 
 const config = configFile;
 
-export const useRollups = () => {
+export const useRollups = (dAddress) => {
   const [contracts, setContracts] = useState();
   const [{ connectedChain }] = useSetChain();
   const [connectedWallet] = useWallets();
+  const [dappAddress] = useState(dAddress);
 
   useEffect(() => {
     const connect = async (chain) => {
-      //@TODO - This provider is causing the errors!!!
-      //hardhat localhost issue, can be ignored
       const provider = new ethers.providers.Web3Provider(
         connectedWallet.provider
       );
-      let address = "0x0000000000000000000000000000000000000000"; //zero addr as placeholder
+      // let address = "0x0000000000000000000000000000000000000000"; //zero addr as placeholder
+      const signer = provider.getSigner();
+      let dappRelayAddress = "";
+      if (config[chain.id]?.DAppRelayAddress) {
+        dappRelayAddress = config[chain.id].DAppRelayAddress;
+      } else {
+        console.error(
+          `No dapp relay address address defined for chain ${chain.id}`
+        );
+      }
+      let inputBoxAddress = "";
+      if (config[chain.id]?.InputBoxAddress) {
+        inputBoxAddress = config[chain.id].InputBoxAddress;
+      } else {
+        console.error(
+          `No input box address address defined for chain ${chain.id}`
+        );
+      }
 
       if (config[chain.id]?.rollupAddress) {
         address = config[chain.id].rollupAddress;
@@ -47,31 +65,65 @@ export const useRollups = () => {
         );
         alert(`No rollup address interface defined for chain ${chain.id}`);
       }
+      let etherPortalAddress = "";
+      if (config[chain.id]?.EtherPortalAddress) {
+        etherPortalAddress = config[chain.id].EtherPortalAddress;
+      } else {
+        console.error(
+          `No ether portal address address defined for chain ${chain.id}`
+        );
+      }
 
-      const rollupsContract = RollupsFacet__factory.connect(
-        address,
-        provider.getSigner()
+      let erc20PortalAddress = "";
+      if (config[chain.id]?.Erc20PortalAddress) {
+        erc20PortalAddress = config[chain.id].Erc20PortalAddress;
+      } else {
+        console.error(
+          `No erc20 portal address address defined for chain ${chain.id}`
+        );
+        alert(`No box erc20 portal address defined for chain ${chain.id}`);
+      }
+
+      let erc721PortalAddress = "";
+      if (config[chain.id]?.Erc721PortalAddress) {
+        erc721PortalAddress = config[chain.id].Erc721PortalAddress;
+      } else {
+        console.error(
+          `No erc721 portal address address defined for chain ${chain.id}`
+        );
+        alert(`No box erc721 portal address defined for chain ${chain.id}`);
+      }
+
+      // dapp contract
+      const dappContract = CartesiDApp__factory.connect(dappAddress, signer);
+
+      // relay contract
+      const realyContract = DAppAddressRelay__factory.connect(
+        dappRelayAddress,
+        signer
       );
+
       // input contract
-      const inputContract = InputFacet__factory.connect(
-        address,
-        provider.getSigner()
-      );
-      const outputContract = OutputFacet__factory.connect(
-        address,
-        provider.getSigner()
+      const inputContract = InputBox__factory.connect(inputBoxAddress, signer);
+
+      // portals contracts
+      const etherPortalContract = EtherPortal__factory.connect(
+        etherPortalAddress,
+        signer
       );
 
-      // const etherPortalContract = EtherPortalFacet__factory.connect(
-      //   address,
-      //   provider.getSigner()
-      // );
+      const erc721PortalContract = ERC721Portal__factory.connect(
+        erc721PortalAddress,
+        signer
+      );
 
       return {
-        rollupsContract,
+        dappContract,
+        signer,
+        realyContract,
         inputContract,
-        outputContract,
-        // etherPortalContract,
+        etherPortalContract,
+        erc721PortalContract,
       };
     };
     if (connectedWallet) {
@@ -81,6 +133,6 @@ export const useRollups = () => {
         });
       }
     }
-  }, [connectedWallet, connectedChain]);
+  }, [connectedWallet, connectedChain, dappAddress]);
   return contracts;
 };
