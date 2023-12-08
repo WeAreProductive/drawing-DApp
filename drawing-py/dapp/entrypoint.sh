055d3532026a4d6be0a08ef47a1f4cdc191dbe3d
Dimitar Angelkov <dimitar.angelkov@gmail.com>
2023-12-08 19:21:18 +0200

    Update entrypoint.sh

--- drawing-py/dapp/entrypoint.sh
+++ drawing-py/dapp/entrypoint.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-export PYTHONPATH=/opt/venv/lib/python3.10/site-packages:/usr/lib/python3/dist-packages
+export PYTHONPATH=/opt/venv/lib/python3.11/site-packages:/usr/lib/python3/dist-packages
 python3 drawing.py
