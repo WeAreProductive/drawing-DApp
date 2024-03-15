ffe648e17e90ecbc9163f9b9b77da2577665c397
Milena Tomova <kokolina1888@users.noreply.github.com>
2024-03-15 14:22:41 +0200

    handle_advance: py to js translation

--- drawing-js/README.md
+++ drawing-js/README.md
@@ -9,3 +9,7 @@ The application entrypoint is the `src/index.js` file. It is bundled with [esbui
 yarn
 ROLLUP_HTTP_SERVER_URL=http://localhost:8080/host-runner node src/index.js
 ```
+
+# ethers.js
+in version 6, utils doesn't exist as all the utils methods are now part of the ethers object, also dont forget there are some breaking changes, [check more on the migration page](https://docs.ethers.org/v6/migrating/#migrate-utils)
+
--- drawing-js/src/index.js
+++ drawing-js/src/index.js
@@ -1,14 +1,82 @@
 // XXX even though ethers is not used in the code below, it's very likely
 // it will be used by any DApp, so we are already including it here
-const { ethers } = require("ethers");
-
-// const rollup_server = process.env.ROLLUP_HTTP_SERVER_URL;
-const rollup_server = "http://localhost:8080/host-runner"
-console.log("HTTP rollup_server url is " + rollup_server);
-
+const { ethers } = require("ethers"); 
+// @TODO convert all variable and func names to camelCase 
+const rollup_server = process.env.ROLLUP_HTTP_SERVER_URL; 
+console.log("HTTP rollup_server url is " + rollup_server); 
+/**
+ * 
+ * @param {*} sender 
+ * @param {*} uuid 
+ * @param {*} erc721_to_mint 
+ * @param {*} selector 
+ * @param {*} imageIPFSMeta 
+ * @param {*} drawing_input 
+ * @param {*} cmd 
+ */
+const mint_erc721_with_string = (
+  sender,
+  uuid, 
+  erc721_to_mint,
+  selector,
+  imageIPFSMeta, 
+  drawing_input, 
+  cmd
+  ) => {
+console.log('mint erc721 with string')
+}
+const  storeDrawingData = ( sender, uuid, drawing_input, cmd ) => { 
+  console.log('store input')
+}
 async function handle_advance(data) {
-  console.log("Received advance request data " + JSON.stringify(data));
-  return "accept";
+  console.log("Received advance request");
+  // console.log("Received advance request data " + JSON.stringify(data));
+  const status = "accept";
+  let payload;
+  const sender = data.metadata.msg_sender.toLowerCase(); 
+  try {
+    payload = data.payload;
+    payload = ethers.utils.toUtf8String(payload); // Decodes a hex string into a regular string.
+    try {
+      console.log('Trying to decode json');
+      // try json data
+      const jsonData = JSON.parse(payload);
+      const {cmd, imageIPFSMeta, erc721_to_mint, selector, uuid, drawing_input} = jsonData;
+      if (cmd){
+        if (cmd == 'cv' || cmd == 'uv'){
+          console.log(`COMMAND ${cmd}`)
+          if (imageIPFSMeta && erc721_to_mint && selector){
+            mint_erc721_with_string( 
+              sender,
+              uuid, 
+              erc721_to_mint,
+              selector,
+              imageIPFSMeta, 
+              drawing_input, 
+              cmd
+            );
+          }
+        } else if (cmd == 'cn' || cmd== 'un'){
+          console.log(`COMMAND ${cmd}`);
+          if (drawing_input){
+            storeDrawingData(
+              sender,
+              uuid,
+              drawing_input, 
+              cmd
+            )
+          }
+        } 
+      } else { // no cmd provided
+        throw Error('Not supported json operation');
+      }
+    } catch (error) {
+      console.log({error})
+    }
+  } catch (error) {
+    console.log(error)
+  }
+  return status;
 }
 
 async function handle_inspect(data) {
