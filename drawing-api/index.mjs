20ab23591b3a9b32c948c1e463df308a082c7cd2
Dimitar Angelkov <dimitar.angelkov@gmail.com>
2024-03-18 16:21:06 +0200

    clean up

--- drawing-api/index.mjs
+++ drawing-api/index.mjs
@@ -2,11 +2,9 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import fs from "fs";
-
-// import { fabric } from "fabric"; // v5
 import { TatumSDK, Network } from "@tatumio/tatum";
 import { API_ENDPOINTS, ORIGIN_BASE, TATUM_KEY } from "./config.mjs";
-import sharp from 'sharp'
+import sharp from "sharp";
 
 const port = 3000;
 const corsOptions = {
@@ -20,14 +18,13 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: "1mb" })); //@TODO - set here proper limit to allow canvas to be saved properly
 app.use(express.static("public")); //make the images accessible by the drawing-ui
 
-function toBase64(filePath) { 
+function toBase64(filePath) {
   const img = fs.readFileSync(`${filePath}`);
   return Buffer.from(img).toString("base64");
 }
 
 const tatumClient = await TatumSDK.init({
   network: Network.Polygon,
-  // network: Network.POLYGON,
   verbose: true,
   apiKey: {
     v4: TATUM_KEY,
@@ -44,53 +41,62 @@ app.post(API_ENDPOINTS.canvasStore, async (req, res) => {
     // @TODO setup canvas metadata to be the same as when Save btn is clicked, drawing-ui
 
     try {
-      
-        if (!fs.existsSync(fullPath)) {
-          fs.mkdirSync(fullPath, { recursive: true });
-          console.log(`Directory ${subDir} created successfully`);
-        }
-      
+      if (!fs.existsSync(fullPath)) {
+        fs.mkdirSync(fullPath, { recursive: true });
+        console.log(`Directory ${subDir} created successfully`);
+      }
+
       //# create .png and save it on disk
       sharp(Buffer.from(req.body.image))
-      .png()
-      .toFile(filePath)
-      .then(async (info)=>{ 
-        const base64String = toBase64(filePath);
-    
-     
-        const buffer = fs.readFileSync(filePath); 
-      // returns the CID of the stored data
-      const imageIPFS = await tatumClient.ipfs.uploadFile({
-        file: buffer,
-      });
+        .png()
+        .toFile(filePath)
+        .then(async (info) => {
+          const base64String = toBase64(filePath);
 
-      const metaData = JSON.stringify({
-        name: "Cartesi Drawing Board NFT",
-        description:
-          "Collaborative drawings powered by Cartesi Rollups and Sunodo.",
-        // image: "ipfs://" + imageIPFS.data.ipfsHash,
-        image: "https://gateway.pinata.cloud/ipfs/" + imageIPFS.data.ipfsHash
-      });
-      console.log(`Check the uploaded image: https://ipfs.io/ipfs/${imageIPFS.data.ipfsHash}`)
-      console.log(`Check the uploaded image: https://gateway.pinata.cloud/ipfs/${imageIPFS.data.ipfsHash}`)
-      const metaIPFS = await tatumClient.ipfs.uploadFile({
-        file: metaData,
-      });
-      console.log(`Check the uploaded file: https://ipfs.io/ipfs/${metaIPFS.data.ipfsHash}`)
-      console.log(`Check the uploaded file: https://gateway.pinata.cloud/ipfs/${metaIPFS.data.ipfsHash}`)
+          const buffer = fs.readFileSync(filePath);
+          // returns the CID of the stored data
+          const imageIPFS = await tatumClient.ipfs.uploadFile({
+            file: buffer,
+          });
 
+          const metaData = JSON.stringify({
+            name: "Cartesi Drawing Board NFT",
+            description:
+              "Collaborative drawings powered by Cartesi Rollups and Sunodo.",
+            // image: "ipfs://" + imageIPFS.data.ipfsHash,
+            image:
+              "https://gateway.pinata.cloud/ipfs/" + imageIPFS.data.ipfsHash,
+          });
+          console.log(
+            `Check the uploaded image: https://ipfs.io/ipfs/${imageIPFS.data.ipfsHash}`
+          );
+          console.log(
+            `Check the uploaded image: https://gateway.pinata.cloud/ipfs/${imageIPFS.data.ipfsHash}`
+          );
 
-      res.send(
-        JSON.stringify({
-          success: true,
-          base64out: base64String,
-          ipfsHash: metaIPFS.data.ipfsHash,
+          const metaIPFS = await tatumClient.ipfs.uploadFile({
+            file: metaData,
+          });
+          console.log(
+            `Check the uploaded meta: https://ipfs.io/ipfs/${metaIPFS.data.ipfsHash}`
+          );
+          console.log(
+            `Check the uploaded meta: https://gateway.pinata.cloud/ipfs/${metaIPFS.data.ipfsHash}`
+          );
+
+          res.send(
+            JSON.stringify({
+              success: true,
+              base64out: base64String,
+              ipfsHash: metaIPFS.data.ipfsHash,
+            })
+          );
+
+          tatumClient.destroy();
         })
-      );
-
-      tatumClient.destroy();
-      })
-      .catch(err => { console.log(err) });
+        .catch((err) => {
+          console.log(err);
+        });
     } catch (error) {
       console.log(error);
     }
