9b54db41aacf41eaf1320fede061e5a560790bf6
Milena Tomova <kokolina1888@users.noreply.github.com>
2024-04-16 12:32:24 +0300

    Display info when unsupported network is selected

--- drawing-ui/src/layouts/Page.tsx
+++ drawing-ui/src/layouts/Page.tsx
@@ -1,14 +1,17 @@
-import { init, useConnectWallet } from "@web3-onboard/react";
+import { init, useConnectWallet, useSetChain } from "@web3-onboard/react";
 import injectedModule from "@web3-onboard/injected-wallets";
-import Network from "../components/Network";
+import NetworkConnect from "../components/NetworkConnect";
 import blocknativeIcon from "../icons/blocknative-icon";
 
 import "../App.css";
 
 import configFile from "../config/config.json";
 import Header from "../components/Header";
+import { Network } from "../shared/types";
+import { toast } from "sonner";
+import { useEffect } from "react";
 
-const config = configFile;
+const config: { [name: string]: Network } = configFile;
 
 const injected = injectedModule();
 
@@ -48,11 +51,29 @@ type Props = {
 
 export default function Page({ children }: Props) {
   const [{ wallet }] = useConnectWallet();
+  const [{ connectedChain }] = useSetChain();
+
+  const check = () => {
+    if (connectedChain) {
+      if (!config[connectedChain?.id]) {
+        let networksList = "";
+        for (let ind in config) {
+          networksList += ` ${config[ind].label}`;
+        }
+        toast.success("Unsupported network connected!", {
+          description: `Please select from the list: ${networksList}`,
+        });
+      }
+    }
+  };
+  useEffect(() => {
+    check();
+  }, [connectedChain?.id]);
   return (
     <div className="flex h-svh flex-col overflow-auto bg-muted">
       <Header />
       <div className="container max-w-none">
-        {wallet ? children : <Network />}
+        {wallet ? children : <NetworkConnect />}
       </div>
     </div>
   );
