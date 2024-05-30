11462d79ecd440e22e19ea27205a1594625da326
Milena Tomova <kokolina1888@users.noreply.github.com>
2024-05-30 10:56:10 +0300

    Remove select mode on double click

--- drawing-ui/src/components/CanvasObjectsControl.tsx/ModeControl.tsx
+++ drawing-ui/src/components/CanvasObjectsControl.tsx/ModeControl.tsx
@@ -3,14 +3,9 @@ import { useCanvasContext } from "../../context/CanvasContext";
 import { Button } from "../ui/button";
 import Draw from "../ui/icons/draw";
 import Select from "../ui/icons/select";
-import { getCursorSvg } from "../../utils";
-import {
-  CANVAS_CURSOR_TYPES,
-  INITIAL_DRAWING_OPTIONS,
-} from "../../shared/constants";
 
 const ModeControl = () => {
-  const { canvas, canvasOptions } = useCanvasContext();
+  const { canvas } = useCanvasContext();
   const [selectionEnabled, setSelectionEnabled] = useState(false);
   const toggleDrawingMode = () => {
     if (!canvas) return;
@@ -21,50 +16,28 @@ const ModeControl = () => {
   };
   useEffect(() => {
     if (!canvas) return;
-    canvas.on("mouse:dblclick", () => {
-      canvas.discardActiveObject();
-      canvas.renderAll();
-
-      canvas.isDrawingMode = !canvas.isDrawingMode;
-      const brushSize =
-        canvasOptions.lineWidth || INITIAL_DRAWING_OPTIONS.minBrushWidth;
-      const getDrawCursor = (cursorType: string) => {
-        const cursor = getCursorSvg(brushSize, canvasOptions.color, cursorType);
-        if (!cursor) return;
-        return `data:image/svg+xml;base64,${window.btoa(cursor)}`;
-      };
-      let cursorType;
-      if (canvas.isDrawingMode) {
-        cursorType = CANVAS_CURSOR_TYPES.circle;
-      } else {
-        cursorType = CANVAS_CURSOR_TYPES.select;
-      }
-      // set custom cursor
-      const cursor = `url(${getDrawCursor(cursorType)}) ${brushSize / 2} ${
-        brushSize / 2
-      }, crosshair`;
-      canvas.setCursor(cursor);
-      setSelectionEnabled(!canvas.isDrawingMode);
-    });
-
     canvas.on("mouse:down", () => {
       if (canvas.isDrawingMode) return;
       const obj = canvas.getActiveObject();
       if (!obj) return;
       // fabricjs.com/docs/fabric.Object.html#__corner
+      // make more visible selected object's border and controls
       obj.set({
         borderScaleFactor: 3,
         // borderColor: "red",
         // cornerColor: "red",
         transparentCorners: false,
-        // borderDashArray:
+        // borderDashArray: ...
       });
     });
-
     return () => {
       canvas.removeListeners();
     };
   }, [canvas]);
+  useEffect(() => {
+    if (!canvas) return;
+    setSelectionEnabled(!canvas.isDrawingMode);
+  }, [canvas, canvas?.isDrawingMode]);
   return (
     <Button variant={"outline"} onClick={toggleDrawingMode}>
       {selectionEnabled ? <Draw /> : <Select />}
