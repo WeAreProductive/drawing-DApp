5ec32aff0763b6c9be009f83393fc055c6affd48
Milena Tomova <kokolina1888@gmail.com>
2024-09-12 09:54:21 +0300

    docker initial setup

---
+++ drawing-rollups-py/sqlite.py
@@ -0,0 +1,29 @@
+import sqlite3 
+
+def init_sqlite_database(filename):
+    """ create a database connection to an SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(filename)
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS drawing (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                owner TEXT NOT NULL,
+            )
+            """
+        )
+
+        conn.commit()
+        print(sqlite3.sqlite_version)
+    except sqlite3.Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+
+if __name__ == '__main__':
+    init_sqlite_database("drawing.db") 
