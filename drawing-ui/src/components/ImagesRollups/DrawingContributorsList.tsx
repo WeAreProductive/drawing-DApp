8d2409e5278c160bb6ef7e9ca040ecafeda0397a
Milena Tomova <kokolina1888@gmail.com>
2024-10-08 16:08:53 +0300

    Display drawing contributors data

--- drawing-ui/src/components/ImagesRollups/DrawingContributorsList.tsx
+++ drawing-ui/src/components/ImagesRollups/DrawingContributorsList.tsx
@@ -1,13 +1,33 @@
-import CanvasSnapshot from "./CanvasSnapshot";
+import { Key } from "react";
 import { useCanvasContext } from "../../context/CanvasContext";
+import DrawingPreview from "./DrawingPreview";
 
 const DrawingContributorsList = () => {
   const { currentDrawingData } = useCanvasContext();
-  console.log({ currentDrawingData });
+
   return (
     currentDrawingData && (
       <div className="rounded-xl bg-card p-6">
-        <div className="-mx-1 flex flex-wrap">drawing contributors list</div>
+        <div className="-mx-1 flex flex-wrap">
+          {currentDrawingData.update_log.map(
+            (element: string, idx: Key | null | undefined) => {
+              // each element is array of `drawing_objects and the painter`
+              const parsedElement = JSON.parse(element[0]);
+              const snapShotJson = JSON.stringify({
+                objects: parsedElement,
+              });
+              return (
+                <div key={idx} className="m-1 border">
+                  <DrawingPreview
+                    dimensions={JSON.parse(currentDrawingData.dimensions)}
+                    snapShotJson={snapShotJson}
+                  />
+                  Painter: {element[1]}
+                </div>
+              );
+            },
+          )}
+        </div>
       </div>
     )
   );
