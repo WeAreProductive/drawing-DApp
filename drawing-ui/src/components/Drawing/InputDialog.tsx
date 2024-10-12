76b9502f44598431348b85055d9769aee571ab77
Milena Tomova <kokolina1888@gmail.com>
2024-10-12 13:19:42 +0300

    Drawing data form initial setup

--- drawing-ui/src/components/Drawing/InputDialog.tsx
+++ drawing-ui/src/components/Drawing/InputDialog.tsx
@@ -1,73 +1,73 @@
-import { Button, Checkbox, Label, Modal, TextInput } from "flowbite-react";
+import {
+  Button,
+  Checkbox,
+  Label,
+  Modal,
+  TextInput,
+  Textarea,
+} from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
 
 const InputDialog = ({ isOpen }) => {
   const [openModal, setOpenModal] = useState(false);
   console.log({ isOpen });
   console.log({ openModal });
-  const emailInputRef = useRef<HTMLInputElement>(null);
+  const titleInputRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
     setOpenModal(isOpen);
   }, [isOpen]);
   return (
     <>
-      {/* <Button onClick={() => setOpenModal(true)}>Toggle modal</Button> */}
       <Modal
         show={openModal}
         size="md"
         popup
         onClose={() => setOpenModal(false)}
-        initialFocus={emailInputRef}
+        initialFocus={titleInputRef}
       >
         <Modal.Header />
         <Modal.Body>
           <div className="space-y-6">
             <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-              Sign in to our platform
+              Give us more info about your drawing:
             </h3>
             <div>
               <div className="mb-2 block">
-                <Label htmlFor="email" value="Your email" />
+                <Label htmlFor="title" value="Drawing title" />
               </div>
               <TextInput
-                id="email"
-                ref={emailInputRef}
-                placeholder="name@company.com"
+                id="title"
+                ref={titleInputRef}
+                placeholder="Drawing title ..."
                 required
               />
             </div>
             <div>
               <div className="mb-2 block">
-                <Label htmlFor="password" value="Your password" />
+                <Label htmlFor="description" value="Drawing description" />
               </div>
-              <TextInput id="password" type="password" required />
+
+              <Textarea id="description"></Textarea>
             </div>
-            <div className="flex justify-between">
-              <div className="flex items-center gap-2">
-                <Checkbox id="remember" />
-                <Label htmlFor="remember">Remember me</Label>
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="price" value="Minting Price" />
               </div>
-              <a
-                href="#"
-                className="text-sm text-cyan-700 hover:underline dark:text-cyan-500"
-              >
-                Lost Password?
-              </a>
+              {/* Number input */}
+              <TextInput id="price" placeholder="Minting price" required />
             </div>
-            <div className="w-full">
-              <Button>Log in to your account</Button>
-            </div>
-            <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
-              Not registered?&nbsp;
-              <a
-                href="#"
-                className="text-cyan-700 hover:underline dark:text-cyan-500"
-              >
-                Create account
-              </a>
-            </div>
+            <div>public/private radio buttons</div>
           </div>
         </Modal.Body>
+        <Modal.Footer>
+          {/* add handler onClick continue*/}
+          <Button color="green" onClick={() => setOpenModal(false)}>
+            Continue
+          </Button>
+          <Button color="gray" onClick={() => setOpenModal(false)}>
+            Decline
+          </Button>
+        </Modal.Footer>
       </Modal>
     </>
   );
