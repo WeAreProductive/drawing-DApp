74756526be090d11de3d7a4a5f427cdcb28a4a14
Milena Tomova <kokolina1888@gmail.com>
2024-10-30 16:57:17 +0200

    Refractor Vouchers

--- drawing-ui/src/components/Vouchers/Vouchers.tsx
+++ drawing-ui/src/components/Vouchers/Vouchers.tsx
@@ -2,36 +2,30 @@ import { Menu } from "lucide-react";
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "../ui/sheet";
 import VouchersList from "./VouchersList";
-import { GraphQLProvider } from "../../context/GraphQLContext";
-import { VouchersView } from "./VouchersView";
 const Vouchers = () => {
   return (
-    <GraphQLProvider>
-      <Sheet>
-        <SheetTrigger>
-          <div className="flex items-center gap-2 text-sm font-semibold">
-            <Menu strokeWidth={1.5} className="h-6 w-6" />
-            My NFTs
-          </div>
-        </SheetTrigger>
-        <SheetContent
-          side="left"
-          className="overflow-y-auto sm:w-[640px] sm:max-w-full"
-        >
-          <SheetHeader>
-            <SheetTitle>My Vouchers</SheetTitle>
-            <VouchersList />
-            {/* <VouchersView /> */}
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
-    </GraphQLProvider>
+    <Sheet>
+      <SheetTrigger>
+        <div className="flex items-center gap-2 text-sm font-semibold">
+          <Menu strokeWidth={1.5} className="h-6 w-6" />
+          My NFTs
+        </div>
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="overflow-y-auto sm:w-[640px] sm:max-w-full"
+      >
+        <SheetHeader>
+          <SheetTitle>My Vouchers</SheetTitle>
+          <VouchersList />
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
   );
 };
 
