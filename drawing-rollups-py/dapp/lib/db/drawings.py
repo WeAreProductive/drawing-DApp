74309e19d11d0e0ab0b731fd08c0122289f3f2e7
Milena Tomova <kokolina1888@gmail.com>
2024-11-25 20:23:02 +0200

    Decompress data with proper decoding

--- drawing-rollups-py/dapp/lib/db/drawings.py
+++ drawing-rollups-py/dapp/lib/db/drawings.py
@@ -166,7 +166,7 @@ def get_drawing_layers(id) :
       current_log['painter'] = row['painter']
       # decompress before sending response data
       decompressed_drawing_objects = zlib.decompress(row['drawing_objects'])
-      current_log['drawing_objects'] = decompressed_drawing_objects
+      current_log['drawing_objects'] = decompressed_drawing_objects.decode("utf-8")
       current_log['dimensions'] = row['dimensions']
 
       update_log.append(current_log)
